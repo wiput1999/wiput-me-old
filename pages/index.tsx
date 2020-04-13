5c6c891cc77f47dc6a830826227b7a9ad16e4ada
Wiput Pootong <wiput.pootong@gmail.com>
2020-04-13 16:59:18 +0700

    :sparkles: add name

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
 import { NextPage } from 'next'
+import { Flex, Heading, Stack } from '@chakra-ui/core'
 
 const IndexPage: NextPage = () => {
-  return <React.Fragment>wiput.me</React.Fragment>
+  return (
+    <Flex
+      overflow='hidden'
+      height='100%'
+      justify='center'
+      align='center'
+      wrap={['wrap', 'wrap', 'initial']}>
+      <Stack spacing={1}>
+        <Heading as='h1' size='2xl'>
+          WIPUT POOTONG
+        </Heading>
+        <Heading as='h4' size='md' textAlign='center'>
+          Full-stack developer
+        </Heading>
+      </Stack>
+    </Flex>
+  )
 }
 
 export default IndexPage
