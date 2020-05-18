b96c6d189c0aca4f8477699b41d24186205577aa
Wiput Pootong <wiput.pootong@gmail.com>
2020-05-18 16:57:28 +0700

    :zap: improve performance

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,38 +4,40 @@ import { NextPage } from 'next'
 
 const IndexPage: NextPage = () => {
   return (
-    <div className='w-full min-h-screen grid grid-cols-1 justify-center items-center'>
-      <div className='w-full text-center'>
-        <p className='text-4xl'>WIPUT POOTONG</p>
-        <p className='text-xl'>Back-end Developer</p>
-        <div className='flex flex-row justify-between mt-4 md:mt-8 px-4 mx-auto max-w-lg underline'>
-          <a
-            href='https://cv.wiput.me'
-            target='_blank'
-            rel='noopener noreferrer'>
-            About me
-          </a>
-          <a
-            href='https://blog.wiput.me'
-            target='_blank'
-            rel='noopener noreferrer'>
-            Blog
-          </a>
-          <a
-            href='https://github.com/wiput1999'
-            target='_blank'
-            rel='noopener noreferrer'>
-            GitHub
-          </a>
-          <a
-            href='https://www.linkedin.com/in/wiputpootong/'
-            target='_blank'
-            rel='noopener noreferrer'>
-            LinkedIn
-          </a>
+    <main>
+      <div className='w-full min-h-screen grid grid-cols-1 justify-center items-center'>
+        <div className='w-full text-center'>
+          <p className='text-4xl'>WIPUT POOTONG</p>
+          <p className='text-xl'>Back-end Developer</p>
+          <div className='flex flex-row justify-between mt-4 md:mt-8 px-4 mx-auto max-w-lg underline'>
+            <a
+              href='https://cv.wiput.me'
+              target='_blank'
+              rel='noopener noreferrer'>
+              About me
+            </a>
+            <a
+              href='https://blog.wiput.me'
+              target='_blank'
+              rel='noopener noreferrer'>
+              Blog
+            </a>
+            <a
+              href='https://github.com/wiput1999'
+              target='_blank'
+              rel='noopener noreferrer'>
+              GitHub
+            </a>
+            <a
+              href='https://www.linkedin.com/in/wiputpootong/'
+              target='_blank'
+              rel='noopener noreferrer'>
+              LinkedIn
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </main>
   )
 }
 
