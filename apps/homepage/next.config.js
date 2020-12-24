a43e2a3889b22a879bbb9b2860c17e2a080528fd
Wiput Pootong <wiput.pootong@gmail.com>
2020-12-25 02:37:10 +0700

    feat: init new version

---
+++ apps/homepage/next.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withNx = require('@nrwl/next/plugins/with-nx');
+const withPlugins = require('next-compose-plugins');
+
+module.exports = withPlugins(
+  [
+    [withNx]
+  ]
+)
