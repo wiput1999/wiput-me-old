fb5d3c86146acf8c893e9e68195a3111f5d5c196
Wiput Pootong <wiput.pootong@gmail.com>
2020-12-25 14:56:43 +0700

    ci: debug

--- tools/plugins/netlify/stop-build/index.js
+++ tools/plugins/netlify/stop-build/index.js
@@ -22,6 +22,7 @@ function projectChanged(currentProject, fromHash, toHash) {
   const output = execSync(getAffected).toString();
   //get the list of changed projects from the output
   const changedProjects = JSON.parse(output).projects;
+  console.log(changedProjects)
   if (changedProjects.find(project => project === currentProject)) {
     return true;
   } else {
