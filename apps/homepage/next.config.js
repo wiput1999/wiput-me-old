54e5e611b7ecbc0c6aba8daa7c737218a8284516
Wiput Pootong <wiput.pootong@gmail.com>
2020-12-27 22:11:29 +0700

    remove: unused config

--- apps/homepage/next.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const withNx = require('@nrwl/next/plugins/with-nx');
-const withPlugins = require('next-compose-plugins');
-
-module.exports = withPlugins(
-  [
-    [withNx]
-  ],
-)
