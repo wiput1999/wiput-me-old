a311e2ee8e25116771060a994f102a4f213ea74a
Wiput Pootong <wiput.pootong@gmail.com>
2022-06-18 22:44:11 +0700

    fix: tailwind

--- apps/cv/tailwind.config.js
+++ apps/cv/tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
   presets: [require('../../tailwind-workspace-preset.js')],
   purge: [
     join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
+    join(__dirname, 'components/**/*.{js,ts,jsx,tsx}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   darkMode: 'media', // or 'media' or 'class'
--- apps/homepage/tailwind.config.js
+++ apps/homepage/tailwind.config.js
@@ -7,9 +7,9 @@ module.exports = {
   presets: [require('../../tailwind-workspace-preset.js')],
   purge: [
     join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
+    join(__dirname, 'components/**/*.{js,ts,jsx,tsx}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
-  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
