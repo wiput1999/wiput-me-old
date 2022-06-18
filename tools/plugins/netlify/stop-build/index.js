1dac029144dece202f364736490a2186becefdfc
Wiput Pootong <wiput.pootong@gmail.com>
2022-06-18 19:21:05 +0700

    fix: deploy

--- tools/plugins/netlify/stop-build/index.js
+++ tools/plugins/netlify/stop-build/index.js
@@ -1,31 +1,31 @@
 module.exports = {
   onPreBuild: ({ utils }) => {
-    const currentProject = process.env.PROJECT_NAME;
-    const lastDeployedCommit = process.env.CACHED_COMMIT_REF;
-    const currentCommit = process.env.COMMIT_REF;
-    const latestCommit = 'HEAD';
+    const currentProject = process.env.PROJECT_NAME
+    const lastDeployedCommit = process.env.CACHED_COMMIT_REF
+    const currentCommit = process.env.COMMIT_REF
+    const latestCommit = 'HEAD'
     const projectHasChanged = projectChanged(
       currentProject,
       lastDeployedCommit,
-      latestCommit
-    );
+      latestCommit,
+    )
     if (!projectHasChanged && currentCommit !== lastDeployedCommit) {
       utils.build.cancelBuild(
-        `Build was cancelled because ${currentProject} was not affected by the latest changes`
-      );
+        `Build was cancelled because ${currentProject} was not affected by the latest changes`,
+      )
     }
-  }
-};
+  },
+}
 
 function projectChanged(currentProject, fromHash, toHash) {
-  const execSync = require('child_process').execSync;
-  const getAffected = `yarn --silent nx print-affected --base=${fromHash} --head=${toHash}`;
-  const output = execSync(getAffected).toString();
+  const execSync = require('child_process').execSync
+  const getAffected = `npx pnpm --silent nx print-affected --base=${fromHash} --head=${toHash}`
+  const output = execSync(getAffected).toString()
   //get the list of changed projects from the output
-  const changedProjects = JSON.parse(output).projects;
-  if (changedProjects.find(project => project === currentProject)) {
-    return true;
+  const changedProjects = JSON.parse(output).projects
+  if (changedProjects.find((project) => project === currentProject)) {
+    return true
   } else {
-    return false;
+    return false
   }
 }
